1fa932926c058bb3de27fa3237193526b3215bc3
Matt <m.stanton300193@gmail.com>
2020-07-11 13:05:23 +0100

    preload sheet initialised

--- src/scenes/_preload.scene.js
+++ src/scenes/_preload.scene.js
@@ -1,3 +1,13 @@
+import { SCENE_NAMES } from "../_cst";
+
 export class PreloadScene extends Phaser.Scene {
   // Preload images and animations here
+
+  constructor() {
+    super(SCENE_NAMES.PRELOAD)
+  }
+
+  preload() {
+    
+  }
 }
