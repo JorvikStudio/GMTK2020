a07450979b842ceae1b3bbf6cf30ff525b9b378b
Chris Jamison <cmj91uk@users.noreply.github.com>
2020-07-11 15:01:57 +0100

    Update menu to use darker walls and scale better

--- src/scenes/menu.scene.js
+++ src/scenes/menu.scene.js
@@ -1,6 +1,5 @@
 import { SCENE_NAMES } from "../_cst";
-import walls from "../assets/castle/walls.png"
-import background from "../assets/castle/background.png"
+import walls from "../assets/castle/walls_far.png"
 import lights3 from "../assets/castle/anim_light3.png";
 
 import { Light } from "../sprites/light/light"
@@ -19,7 +18,6 @@ export class MainMenu extends Phaser.Scene {
 
     preload() {
         this.load.image("wall", walls);
-        this.load.image("menu_background", background);
         this.load.spritesheet("light_animation", lights3, {
             frameWidth: 64,
             frameHeight: 64
@@ -86,25 +84,18 @@ export class MainMenu extends Phaser.Scene {
 
     renderBackground(gameWidth, gameHeight) {
      
-        const tileScale = 2;
         const backgroundWidth = 480;
         const backgroundHeight = 240;
         const imageWidth = 128;
         const imageHeight = 256;
-        const tileY = gameHeight - (imageHeight * tileScale);
 
-        const backgroundScale = Math.max(gameWidth / backgroundWidth, gameHeight / backgroundHeight);
-        console.log(`Background scale is ${backgroundScale}`);
-
-        this.add.image(0, 0, "menu_background")
-            .setDisplayOrigin(0, 0)
-            .setScale(backgroundScale);
-
+        const tileScale = gameHeight / imageHeight;
+        const tileY = 0;
         
         const columns = Math.ceil(gameWidth / imageWidth);
         const imageCropX = 304;
         for (let i = 0; i < columns; i++) {
-            const imageX = ((i * imageWidth) - imageCropX) * 2;
+            const imageX = ((i * imageWidth) - imageCropX - 20) * tileScale; //yuck! Magic number of 20 makes it look symmetrical
             let image = this.add.image(imageX, tileY, "wall")
             image.setDisplayOrigin(0, 0);
             image.setScale(tileScale);
@@ -115,6 +106,5 @@ export class MainMenu extends Phaser.Scene {
     update() {
         this.light.update();
     }
-
 
 }
