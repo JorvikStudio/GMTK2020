95c707372becfb5a171c2ab867ac9d4b826cec8b
LumleyA <LumleyA@users.noreply.github.com>
2020-07-12 17:27:06 +0000

    deploy: 5c5bd1a4277dd0abbebfda34136b489b7908430c

--- src/scenes/level2.scene.js
+++ src/scenes/level2.scene.js
@@ -52,8 +52,8 @@ export class Level2Scene extends Scene {
       this.player = new Player(this, 100, 400);
       this.enemies.add(new Enemy3(this, 500, 410, 0, 0));
       this.enemies.add(new Enemy4(this, 850, 210, 0, 0));
-      this.enemies.add(new Enemy1(this, 1100, 400, 1050, 1300));
-      this.enemies.add(new Enemy5(this, 1350, 400, 1350, 1500));
+      this.enemies.add(new Enemy1(this, 1100, 400, 1000, 1300));
+      this.enemies.add(new Enemy5(this, 1350, 400, 1300, 1500));
 
       // set bounds so the camera won't go outside the game world
       this.cameras.main.setBounds(0, 0, 1600, 610);
