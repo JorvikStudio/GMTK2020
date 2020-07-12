6187ec3f5520be1e0a487c77bda47a48dd7efacc
Matt <m.stanton300193@gmail.com>
2020-07-12 11:07:09 +0100

    Fireball now collides with SOME walls

--- src/sprites/fireball/fireball.js
+++ src/sprites/fireball/fireball.js
@@ -13,7 +13,7 @@ export class Fireball extends Phaser.Physics.Arcade.Sprite {
       this.body.setImmovable(true);
       this.body.allowGravity = false;
       this.body.isCircle = true;
-      
+
       this.pi = 0;
 
       this.createFlightpath();
@@ -21,33 +21,31 @@ export class Fireball extends Phaser.Physics.Arcade.Sprite {
 
   createFlightpath ()
   {
-      this.size = 32;
+    this.size = 32;
 
-      
-      this.curve = new Phaser.Curves.Spline();
-      console.log(this.direction);
-      for (var i = 0; i<8; i++)
-      {
-        this.curve.addPoint((this.x + (60 * i * this.direction)), (this.y + 30*(Math.sin(i * 90 * this.direction))))
-      }        
-      
-      this.size = 32;
-      
-      this.points = this.curve.getDistancePoints(this.size);
-    }
-    
-    // this will draw the flight path, more for debugging than anything else
-    drawFlightpath()
+    this.curve = new Phaser.Curves.Spline();
+
+    for (var i = 0; i<8; i++)
     {
-      this.graphics = this.scene.add.graphics();
-      
-      this.graphics.lineStyle(1, 0xffffff, 1);
+      this.curve.addPoint((this.x + (60 * i * this.direction)), (this.y + 30*(Math.sin(i * 90 * this.direction))))
+    }        
+    
+    this.size = 32;
+    
+    this.points = this.curve.getDistancePoints(this.size);
+  }
+    
+  // this will draw the flight path, more for debugging than anything else
+  drawFlightpath()
+  {
+    this.graphics = this.scene.add.graphics();
+    
+    this.graphics.lineStyle(1, 0xffffff, 1);
 
-      this.curve.draw(this.graphics, 64);
-  
-      this.graphics.fillStyle(0x00ff00, 1);
-      this.graphics.lineStyle(1, 0x00ff00, 1);
+    this.curve.draw(this.graphics, 64);
 
+    this.graphics.fillStyle(0x00ff00, 1);
+    this.graphics.lineStyle(1, 0x00ff00, 1);
   }
 
   create ()
@@ -64,7 +62,7 @@ export class Fireball extends Phaser.Physics.Arcade.Sprite {
       {
           const percent = (this.pi+i)/70;
 
-          if(percent <= 1) {
+          if(percent <= 1 && this.active) {
             const point = this.curve.getPoint(percent);
             if (point) {
 
@@ -74,12 +72,11 @@ export class Fireball extends Phaser.Physics.Arcade.Sprite {
               this.drawfireball(point.x, point.y, size);
               this.body.position = new Phaser.Math.Vector2(point.x - offset, point.y - offset);
               this.body.updateCenter();
+              this.body.setVelocityX(1);
             }
+          } else {
+            this.destroySelf();
           }
-      }
-      
-      if(destroySelf) {
-        this.destroy();
       }
       
       this.pi++;
@@ -89,5 +86,15 @@ export class Fireball extends Phaser.Physics.Arcade.Sprite {
   {
       this.ball.fillStyle(0xf0652e, 1);
       this.ball.fillCircle(x, y, size);
+  }
+
+  onImpact() {
+    this.active = false;
+    this.destroySelf();
+  }
+
+  destroySelf() {
+    this.ball.clear();
+    this.destroy();
   }
 }
