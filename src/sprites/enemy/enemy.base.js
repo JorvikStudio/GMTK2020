30623e7a4860d6cc290e6e827ee751b9d6a1c0fd
cmj91uk <cmj91uk@users.noreply.github.com>
2020-07-12 14:12:45 +0000

    deploy: a97bc5f6459f9766da509c5e7a41c8dcb4ee1093

--- src/sprites/enemy/enemy.base.js
+++ src/sprites/enemy/enemy.base.js
@@ -9,6 +9,12 @@ export class EnemyBase extends Phaser.Physics.Arcade.Sprite {
         scene.add.existing(this);
         this.body.setSize(width, height);
         this.scale = 1.5
+        this.hasHitFloor = false;
+        this.checkFloors = false;
+        this.patrolBoundaries = {
+            left: 496,
+            right: 704
+        };
     }
 
     startBaseAnimation() {
@@ -17,5 +23,44 @@ export class EnemyBase extends Phaser.Physics.Arcade.Sprite {
 
     scheduleAnimations() {
         //Implement me in subclass
+    }
+
+    update() {
+        this.patrolPoints();
+    }
+
+    patrolPoints() {
+        if (this.checkFloors) {
+            const currentVelocity = this.body.velocity.x;
+            const isGoingLeft = currentVelocity < 0;
+            const isGoingRight = currentVelocity > 0;
+            const velocityMultiplier = 50;
+            if (!isGoingLeft && !isGoingRight) {
+                const startingVelocity = velocityMultiplier * this.getRandomDirection()
+                this.setVelocityX(startingVelocity);
+                this.setFlipX(startingVelocity < 0);
+            }
+            else if (isGoingLeft && this.body.left - this.body.width - 5 < this.patrolBoundaries.left)
+            {
+                this.setFlipX(false);
+                this.setVelocityX(-1 * currentVelocity);
+            }
+            else if (isGoingRight && this.body.right + 5 > this.patrolBoundaries.right)
+            {
+                this.setFlipX(true);
+                this.setVelocityX(-1 * currentVelocity);
+            }
+        }
+        else {
+            // Start checking floors after 1ms. Otherwise we sometimes fly off in a random direction at game start
+            setTimeout(() => { this.checkFloors = true; }, 1);
+        }
+    }
+
+    getRandomDirection() {
+        const random = Math.random();
+        if (random > 0.5)
+            return 1;
+        return -1;
     }
 }
